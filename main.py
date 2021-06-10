7bd8c6fdbf1ba8312029c6fea6c76782d24840aa
vladb0204 <vladbalandin2013@gmail.com>
2021-06-10 20:08:21 +0300

    Added new function, my second task

--- main.py
+++ main.py
@@ -1,76 +1,91 @@
-from web import *
+# from web import *
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 
-url_main_1 = 'https://clutch.co/uk/app-developers?page='
-url_main_2 = '&related_services=&related_services=field_pp_sl_ecommerce'
 
-columns = ['Company_names', 'Price']
-apps_names_list_main = []
-apps_price_list_main = []
-apps_names_dict = dict()
+def parsing_function_replies():
+    url_main = 'https://apps.shopify.com/plobalapps-mobile-application/reviews?page='
+    columns = ['Name', 'Overall rating', 'Date of reply', 'Text']
 
-for index in range(1, 10):
-    code = requests.get(url_main_1 + str(index) + url_main_2)
-    soup_code = bs(code.text, 'lxml')
+    all_replies_dict = dict()
+    all_replies_info = []
 
-    apps_names_list = soup_code.find_all('h3', {'class': 'company_info'})
-    apps_price_list = soup_code.find_all('div', {'class': 'list-item block_tag custom_popover'})
+    page_index = 1
+    while True:
+        code = requests.get(url_main + str(page_index))
+        if 200 >= code.status_code <= 299:
+            soup_code = bs(code.text, 'lxml')
 
-    # apps_info_names = soup_code.find_all('div', {'class': 'h1'})
-    # apps_info_languages = soup_code.find_all('ul', {'class': 'list-inline'})
+            all_replies_found = soup_code.find_all('div', {'class': 'review-listing'})
+            for reply in all_replies_found:
+                all_replies_info.append(reply)
+
+            for index in range(len(all_replies_info)):
+                all_replies_dict[index] = dict()
+                all_replies_dict[index][columns[0]] = all_replies_info[index].find('h3').text.strip().replace('\n', '')
+                all_replies_dict[index][columns[1]] = \
+                    all_replies_info[index].find('span', {'class': 'ui-star-rating__rating visuallyhidden'}).text.replace(' of ', '/')
+                all_replies_dict[index][columns[2]] = \
+                    all_replies_info[index].find_all('div', {'class': 'review-metadata__item-value'})[1].text.strip()\
+                        .replace('\n', '')
+                all_replies_dict[index][columns[3]] = all_replies_info[index].find('p').text.replace('\n', '')
+
+            print(f'Parsing at page number {page_index} completed successfully!')
+            page_index += 1
+
+            data = pd.DataFrame(data=all_replies_dict, index=columns).T
+            data.to_csv('replies_info.csv')
+        else:
+            print(f'This page has not been found, status code of the page: {code.status_code}')
+            break
 
 
+def parsing_function_app():
+    url_main_1 = 'https://clutch.co/uk/app-developers?page='
+    url_main_2 = '&related_services=&related_services=field_pp_sl_ecommerce'
 
-    print(soup_code)
+    columns = ['Company name', 'Price']
+    app_info_dict = dict()
 
-    for app_info in apps_names_list:
-        apps_names_list_main.append(app_info.text.strip().replace('\n', ''))
+    app_names_list = []
+    app_prices_list = []
 
-    print(apps_price_list)
+    page_index = 1
+    while True:
+        code = requests.get(url_main_1 + str(page_index) + url_main_2)
+        if 200 >= code.status_code <= 299:
+            soup_code = bs(code.text, 'lxml')
 
-    apps_names_list_main.append(str(apps_price_list)[
-                                str(apps_price_list).index('<span>$'):str(apps_price_list).index('</span>')])
+            app_names_local = soup_code.find_all('h3', {'class': 'company_info'})
+            app_block_local = soup_code.find_all('div', {'class': 'list-item block_tag custom_popover'})
 
-    for index_1 in range(len(apps_names_list_main)):
-        apps_names_dict[index_1] = dict()
-        apps_names_dict[index_1]['Company_name'] = apps_names_list_main[index_1]
-        apps_names_dict[index_1]['Price'] = apps_price_list_main[0]
+            for block in app_block_local:
+                price_found = block.find('span')
+                app_prices_list.append(price_found)
 
-    print(apps_names_dict)
+            for name in app_names_local:
+                app_names_list.append(name.text.strip().replace('\n', ''))
 
-    data_file = pd.DataFrame(data=apps_names_dict, index=columns).T
-    data_file.to_csv('US_Companies_main.csv')
+            if len(app_prices_list) != len(app_names_list):
+                raise Exception('Lists have different length!')
+            else:
+                for index in range(len(app_prices_list)):
+                    app_info_dict[index] = dict()
+                    app_info_dict[index][columns[0]] = app_names_list[index]
+                    app_info_dict[index][columns[1]] = app_prices_list[index]
 
-    print(f'Page {index} ready')
+                print(f'Parsing at page number {page_index} completed successfully!')
+                page_index += 1
+        else:
+            print(f'This page has not been found, status code of the page: {code.status_code}')
+            break
 
-# data = pd.read_csv('UK_Companies.csv').T
-# data_dict = data.to_dict()
-#
-# new_data = dict()
-# for index in range(len(data_dict)):
-#     new_data[index] = dict()
-#     new_data[index] = data_dict[index]['Company_name']
-#
-# data_file = pd.DataFrame(data=new_data, index=['Company_name']).T
-# data_file.to_csv('UK_Companies_1.csv')
+    data = pd.DataFrame(data=app_info_dict, index=columns).T
+    data.to_csv('uk_companies_info.csv')
+    # data.to_csv('us_companies_info.csv')
 
-# url_main_first = 'https://appropio.com/top-27-luchshih-sredstv-po-dlja-razrabotki-mobilnyh-prilozhenij/'
-# url_main_second = 'https://www.softwareadvice.com/construction/best-apps-comparison/p/all/'
-# url_main_third = 'https://wadline.com/mobile/gb?page='
-#
-# code = requests.get(url_main_third)
-# soup_code = bs(code.text, 'lxml')
-#
-# all_results = []
-# for index in range(1, 4):
-#     print(index)
 
-# apps_info = soup_code.find_all('div', {'class': 'full category'})
-# print(len(apps_info))
-#
-# app_info = apps_info[0].find_all('div')
-# print(len(app_info))
-# for elem in app_info:
-#     print(elem, end='\n\n')
+if __name__ == '__main__':
+    parsing_function_replies()
+    # parsing_function_app()
