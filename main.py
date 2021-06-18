0a836147e6beccd5d80bb1098fa17cbf27ce0ed2
vladb0204 <vladbalandin2013@gmail.com>
2021-06-18 14:14:03 +0300

    Renewed my project (part 3)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from project_files import first_task, second_task, third_task, fourth_task
 
 if __name__ == '__main__':
-    # first_task.compile_task()
+    first_task.compile_task()
     # second_task.compile_task()
     # third_task.compile_task()
     # fourth_task.compile_task()
