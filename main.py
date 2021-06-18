5ab9374bce8f8279e1b89aad8705769250c078bb
vladb0204 <vladbalandin2013@gmail.com>
2021-06-18 14:15:50 +0300

    Just added new file to my project

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from project_files import first_task, second_task, third_task, fourth_task
+from project_files import first_task, second_task, third_task, fourth_task, fifth_task
 
 if __name__ == '__main__':
     first_task.compile_task()
     # second_task.compile_task()
     # third_task.compile_task()
     # fourth_task.compile_task()
+    # fifth_task.compile_task()
